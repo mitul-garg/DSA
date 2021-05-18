ebebb5d6b726c6574d0537fc292ec27336681b09
unknown <mitulgarg2000@gmail.com>
2021-05-18 15:05:26 +0530

    Insertion Sort Done; Merge Sort Started

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -7,8 +7,64 @@ public class Sorting {
 		printArray(arr);
 //		bubbleSort(arr);
 //		optimisedBubbleSort(arr);
-		selectionSort(arr);
+//		selectionSort(arr);
+//		insertionSort(arr);
+//		int[] arr1 = {1, 2, 4, 7, 10};
+//		int[] arr2 = {2, 3, 5, 6, 11};
+//		mergeSortedArrays(arr1, arr2);
+//		mergeSort(arr, 0, arr.length-1);
 		printArray(arr);
+	}
+	
+//	public static void mergeSort (int[] arr, int low, int high) {
+//	O(nlogn) Time and O(n) Aux Space
+//		if (low < high) {
+//			int mid = low + (high - low)/2; // this is to avoid overflow on large values
+//			mergeSort(arr, low, mid);
+//			mergeSort(arr, mid+1, high);
+//			merge(arr, low, mid, high);
+//		}
+//	}
+	
+	public static void mergeSortedArrays(int[] arr1, int[] arr2) {
+		int n = arr1.length, m = arr2.length, i, j, k;
+		int[] ans = new int[n+m];
+		for (i=0,j=0,k=0; i<n && j<m;) {
+			if (arr1[i]<arr2[j]) {
+				ans[k] = arr1[i];
+				i++;
+				k++;
+			}
+			else {
+				ans[k] = arr2[j];
+				j++;
+				k++;
+			}
+		}
+		while (i < n) {
+			ans[k] = arr1[i];
+			i++; 
+			k++;
+		}
+		while (j < m) {
+			ans[k] = arr2[j];
+			j++; 
+			k++;
+		}
+		printArray(ans);
+	}
+	
+	public static void insertionSort (int[] arr) {
+		int n = arr.length;
+		for (int i=1; i<n; i++) {
+			int key = arr[i];
+			int j = i-1;
+			while (j>=0 && arr[j]>key) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
 	}
 	
 	public static void selectionSort (int[] arr) {
